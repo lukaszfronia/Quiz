3a6733b7daacae6d9210e38a309340e16807d77d
lukaszfronia <86720750+lukaszfronia@users.noreply.github.com>
2022-09-21 23:01:51 +0200

    reducing the time for answer

--- dist/config.js
+++ dist/config.js
@@ -1,3 +1,3 @@
-export const TIME_SEC = 90;
+export const TIME_SEC = 30;
 export const TIME_FOR_RELOAD_SEC = 6;
 export const COUNTDOWN_TIME_SEC = 1;
--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
-export const TIME_SEC: number = 90;
+export const TIME_SEC: number = 30;
 export const TIME_FOR_RELOAD_SEC: number = 6;
 export const COUNTDOWN_TIME_SEC: number = 1;
